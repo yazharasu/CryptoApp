f2b9dc0f79a4a42052325597c5fc25479cd1216f
yazharasu <yazh.bis@gmail.com>
2022-02-20 10:53:05 +0530

    Change

---
+++ src/Services/cryptoApi.js
@@ -0,0 +1,31 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '3dac2a064emsh50c77a2ffa0fc86p183d20jsna782d789acd0'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers:cryptoApiHeaders})
+
+export const cryptoApi = createApi( { 
+    reducerPath: 'cryptoApi', 
+    baseQuery: fetchBaseQuery( { baseUrl } ),
+    endpoints: (builder) => ({
+        getCryptos: builder.query( { 
+            query: (count) => createRequest(`/coins?limit=${count}`),
+        }),
+        getCryptoDetails: builder.query( { 
+            query: (uuid) => createRequest(`/coin/${uuid}`),
+        }),
+        getCryptoDetails: builder.query( { 
+            query: (uuid) => createRequest(`/coin/${uuid}`),
+        }),
+        getCryptoHistory: builder.query({
+            query: ({ uuid, timeperiod }) => createRequest(`coin/${uuid}/history?timeperiod=${timeperiod}`),
+          }),
+    })
+});
+
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } = cryptoApi;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,64 @@
+import React, {useEffect, useState} from "react";
+import { Button, Menu, Typography, Avatar } from 'antd';
+import {Link} from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulfOutlined, FundOutlined, MenuOutlined, BulbFilled, BulbOutlined } from '@ant-design/icons';
+import icon from "../images/icon.png";
+
+const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+
+  useEffect( () => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener( 'resize', handleResize);
+  }, []);
+
+  useEffect( () => {
+    if(screenSize <760) {
+      setActiveMenu(false);
+    }else {
+      setActiveMenu(true);
+    }
+  }, [screenSize])
+
+  return (
+    <div className="nav-container">
+        <div className="logo-container">
+            <div>
+              <Avatar src={icon} size='large'/>
+
+              <Typography.Title level={3} className='logo'>
+                  <Link to="/">CryptoUniverse</Link>
+              </Typography.Title>
+
+              <Button className='menu-control-container' onClick={ ()=> setActiveMenu(!activeMenu)} />
+            </div>
+
+            <Menu theme="dark" className="menu-container">
+                <Menu.Item icon={<HomeOutlined />}>
+                  <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined />}>
+                  <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                  <Link to="/exchanges">Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined />}>
+                  <Link to="/news">News</Link>
+                </Menu.Item>
+            </Menu>
+
+            {/* <Button className="menu-container-container">
+
+            </Button> */}
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar;
