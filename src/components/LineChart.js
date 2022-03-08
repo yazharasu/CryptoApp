58bdeaf5cc1c6e484f9563ae1a97de5a18bd1277
Yazharasu <81926673+yazharasu@users.noreply.github.com>
2022-03-08 12:36:36 +0530

    change

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Chart as ChartJS } from 'chart.js/auto'
+// import { Chart as ChartJS } from 'chart.js/auto'
 import { Line } from 'react-chartjs-2';
 import { Col, Row, Typography } from 'antd';
 
